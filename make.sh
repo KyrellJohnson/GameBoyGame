cce4f07b27ba8c82bacfa3f913b4e908fa9960d8
kyrell <kyrelljohnson@yahoo.com>
2023-11-17 16:31:03 -0500

    init

---
+++ make.sh
@@ -0,0 +1,24 @@
+# delete previous files
+mkdir build
+cd build
+rm *.gb
+cd ..
+
+# Compile .c files into .o files
+cd src
+lcc -c -o *.o *.c
+cd ..
+
+# Compile a GB file from the compiled .o files
+lcc -o build/game.gb src/*.o #src/main.o src/player.o
+
+# Delete intermediate files
+rm src/*.asm
+rm src/*.lst
+rm src/*.ihx
+rm src/*.sym
+rm src/*.o
+rm build/*.ihx
+
+# run the game in the emulator
+snap run sameboy "${PWD}/build/game.gb"
---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <gb/gb.h>
+#include "player.h"
+
+int main(void)
+{
+	uint8_t currentspriteindex = 0;
+
+	set_sprite_data(0, 2, MyPlayer);
+	set_sprite_tile(0, 0);
+	move_sprite(0, 88, 78);
+
+	SHOW_SPRITES;
+
+	while (1)
+	{
+		if (currentspriteindex == 0)
+		{
+			currentspriteindex = 1;
+		}
+		else
+		{
+			currentspriteindex = 0;
+		}
+
+		set_sprite_tile(0, currentspriteindex);
+		delay(1000);
+
+		scroll_sprite(0, 10, 0);
+	}
+
+	return 0;
+}
